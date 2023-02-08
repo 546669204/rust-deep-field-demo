316e253663af85d2e74ae65a8bd28ff80d039a08
546669204 <546669204@qq.com>
2023-02-08 23:16:48 +0800

    .

--- ci/install.sh
+++ ci/install.sh
@@ -10,3 +10,7 @@ export PATH="/C/tools/opencv/build/x64/vc15/bin:$PATH"
 export OPENCV_LINK_PATHS="/C/tools/opencv/build/x64/vc15/lib"
 export OPENCV_LINK_LIBS="opencv_world${OPENCV_VERSION//./}"
 export OPENCV_INCLUDE_PATHS="/C/tools/opencv/build/include"
+
+
+
+cargo build -r
