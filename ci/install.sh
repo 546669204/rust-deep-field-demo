ebc21d4d5f08f9f61eec955a639cc48b82ed42ec
546669204 <546669204@qq.com>
2023-02-08 21:14:19 +0800

    .

---
+++ ci/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -vex
+
+choco install -y llvm 
+choco install -y opencv --version "$OPENCV_VERSION"
+
+
+export PATH="/C/tools/opencv/build/x64/vc15/bin:$PATH"
+export OPENCV_LINK_PATHS="/C/tools/opencv/build/x64/vc15/lib"
+export OPENCV_LINK_LIBS="opencv_world${OPENCV_VERSION//./}"
+export OPENCV_INCLUDE_PATHS="/C/tools/opencv/build/include"
