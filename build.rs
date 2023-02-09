d10b544bad44cc062ba0642a2d76bfb78fb33de4
546669204 <546669204@qq.com>
2023-02-09 15:55:32 +0800

    .

--- build.rs
+++ build.rs
@@ -1,23 +1,28 @@
 use glob::glob;
-use std::{env, fs, path};
+use std::{fs, path};
+
+fn windows_copy_dll(){
+  for entry in glob("target/release/build/torch-sys-*/out/libtorch/libtorch/lib/*.dll")
+  .expect("Failed to read glob pattern")
+  .filter_map(Result::ok)
+  .chain(
+      glob("C:/tools/opencv/build/x64/vc15/bin/*.dll")
+          .expect("Failed to read glob pattern")
+          .filter_map(Result::ok),
+  )
+{
+  fs::copy(
+      &entry.as_os_str(),
+      path::Path::new("./target/release/").join(&entry.file_name().unwrap()),
+  )
+  .unwrap();
+  ()
+}
+}
 
 fn main() {
-  for entry in glob("target/release/build/torch-sys-*/out/libtorch/libtorch/lib/*.dll")
-    .expect("Failed to read glob pattern")
-    .filter_map(Result::ok)
-    .chain(
-        glob("C:/tools/opencv/build/x64/vc15/bin/*.dll")
-            .expect("Failed to read glob pattern")
-            .filter_map(Result::ok),
-    )
-  {
-    fs::copy(
-        &entry.as_os_str(),
-        path::Path::new("./target/release/").join(&entry.file_name().unwrap()),
-    )
-    .unwrap();
-    ()
-  }
-  
-  tauri_build::build()
+    #[cfg(target_os = "windows")]
+    windows_copy_dll();
+
+    tauri_build::build()
 }
