162f18389b0df0db8a02edce316129d6696e4bc1
546669204 <546669204@qq.com>
2023-02-10 17:02:12 +0800

    .

--- ci/install.sh
+++ ci/install.sh
@@ -49,4 +49,4 @@ rm -rf target/release/bundle
 cargo build -r
 
 
-zip –q –r "$OS_FAMILY.zip" -x=target/release/build -x=target/release/bundle -x=target/release/deps -x=target/release/examples -x=target/release/incremental target/release/* 
+zip –q –r "$OS_FAMILY.zip" target/release/* -x target/release/build target/release/bundle target/release/deps target/release/examples target/release/incremental 
