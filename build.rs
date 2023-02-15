1b22320905ed7a8a5ea8fc8d7b7f0335d5a98e24
xiaoming <546669204@qq.com>
2023-02-15 21:49:20 +0800

    feat: build.rs add env profile

--- build.rs
+++ build.rs
@@ -2,42 +2,48 @@ use glob::glob;
 use std::{fs, path};
 
 #[cfg(target_os = "windows")]
-fn windows_copy_dll(){
-  for entry in glob("target/release/build/torch-sys-*/out/libtorch/libtorch/lib/*.dll")
+fn windows_copy_dll() {
+    for entry in glob(&format!(
+        "target/{}/build/torch-sys-*/out/libtorch/libtorch/lib/*.dll",
+        std::env::var("PROFILE").unwrap()
+    ))
     .expect("Failed to read glob pattern")
     .filter_map(Result::ok)
     .chain(
         glob("C:/tools/opencv/build/x64/vc15/bin/*.dll")
             .expect("Failed to read glob pattern")
             .filter_map(Result::ok),
-    )
-  {
-    fs::copy(
-        &entry.as_os_str(),
-        path::Path::new("./target/release/").join(&entry.file_name().unwrap()),
-    )
-    .unwrap();
-    ()
-  }
+    ) {
+        fs::copy(
+            &entry.as_os_str(),
+            path::Path::new(&format!("./target/{}/", std::env::var("PROFILE").unwrap()))
+                .join(&entry.file_name().unwrap()),
+        )
+        .unwrap();
+        ()
+    }
 }
 #[cfg(target_os = "linux")]
-fn linux_copy_so(){
-  for entry in glob("target/release/build/torch-sys-*/out/libtorch/libtorch/lib/*.so")
+fn linux_copy_so() {
+    for entry in glob(&format!(
+        "target/{}/build/torch-sys-*/out/libtorch/libtorch/lib/*.so",
+        std::env::var("PROFILE").unwrap()
+    ))
     .expect("Failed to read glob pattern")
     .filter_map(Result::ok)
     .chain(
         glob("/usr/local/lib/libopencv*.so")
             .expect("Failed to read glob pattern")
             .filter_map(Result::ok),
-    )
-  {
-    fs::copy(
-        &entry.as_os_str(),
-        path::Path::new("./target/release/").join(&entry.file_name().unwrap()),
-    )
-    .unwrap();
-    ()
-  }
+    ) {
+        fs::copy(
+            &entry.as_os_str(),
+            path::Path::new(&format!("./target/{}/", std::env::var("PROFILE").unwrap()))
+                .join(&entry.file_name().unwrap()),
+        )
+        .unwrap();
+        ()
+    }
 }
 
 
